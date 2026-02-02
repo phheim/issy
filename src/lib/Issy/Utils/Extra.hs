5be31531315258b7fa8b2e3c9ca1061f23cc2aae
Philippe Heim <philippe.heim@cispa.de>
2026-02-02 16:22:25 +0100

    documentation

--- src/lib/Issy/Utils/Extra.hs
+++ src/lib/Issy/Utils/Extra.hs
@@ -1,15 +1,20 @@
 ---------------------------------------------------------------------------------------------------
 -- |
 -- Module      : Issy.Utils.Extra
--- Description : TODO DOCUMENT
+-- Description : Common extra functions that are useful but would require more dependencies
 -- Copyright   : (c) Philippe Heim, 2026
 -- License     : The Unlicense
 --
+-- This module implements common extra functions (e.g. for monads) that are however
+-- not part of the base standard library and would therefore increase the dependencies. It also
+-- has a few useful other generic extra functions.
 ---------------------------------------------------------------------------------------------------
 {-# LANGUAGE Safe, LambdaCase #-}
 
+---------------------------------------------------------------------------------------------------
 module Issy.Utils.Extra
-  ( justOn
+  ( -- Maybes, Either, and Monads
+    justOn
   , ifM
   , ifMC
   , ifMD
@@ -18,20 +23,24 @@ module Issy.Utils.Extra
   , orM
   , andM
   , rightToMaybe
-  , predecessorRelation
+  , -- String related operations
+    firstLine
+  , paraInbetween
+  , inbetween
+  , enclose
+  , -- Running external processes
+    runTO
+  , noTimeout
+  , -- Set, Map, and Graph operations
+    predecessorRelation
   , reachables
-  , firstLine
   , intmapSet
   , invertMap
   , mapFromSet
   , mapFromSetWith
-  , runTO
-  , noTimeout
-  , paraInbetween
-  , inbetween
-  , enclose
   ) where
 
+---------------------------------------------------------------------------------------------------
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Set (Set)
@@ -41,11 +50,24 @@ import qualified System.Timeout as Sys (timeout)
 
 import qualified Issy.Utils.OpenList as OL
 
+---------------------------------------------------------------------------------------------------
+-- Maybe, Either, Monads
+---------------------------------------------------------------------------------------------------
+-- | 'Just' of the given element if the condition is 'True'
 justOn :: Bool -> a -> Maybe a
 justOn p
   | p = Just
   | otherwise = const Nothing
 
+-- | Turns a 'Right' value to 'Just', and 'Left' to 'Nothing' discarding the value
+rightToMaybe :: Either a b -> Maybe b
+rightToMaybe =
+  \case
+    Right b -> Just b
+    Left _ -> Nothing
+
+-- | Evaluates the boolean condition and base on the result returns the first (if true) or
+-- second value (if false)
 ifM :: Monad m => m Bool -> m a -> m a -> m a
 ifM b t f = do
   b <- b
@@ -53,12 +75,16 @@ ifM b t f = do
     then t
     else f
 
+-- | Like 'ifM' but the "then-branch" is a pure value
 ifMC :: Monad m => m Bool -> a -> m a -> m a
 ifMC b t = ifM b $ pure t
 
+-- | Like 'ifM' but the "else-branch" is a pure value
 ifMD :: Monad m => m Bool -> m a -> a -> m a
 ifMD b t = ifM b t . pure
 
+-- | Like 'ifM' but the condition returns an additional value that is returned and
+-- both branches are pure.
 ifQuery :: Monad m => m (Bool, a) -> b -> b -> m (b, a)
 ifQuery c t f = do
   (res, a) <- c
@@ -66,58 +92,62 @@ ifQuery c t f = do
     then pure (t, a)
     else pure (f, a)
 
+-- | Check if the given predicate holds on all elements. Stops early is 'False'
+-- is encountered and evaluates in the order of 'foldl'
 allM :: (Foldable f, Monad m) => (a -> m Bool) -> f a -> m Bool
 allM pred = foldl (\acc elem -> ifM acc (pred elem) (pure False)) (pure True)
 
+-- | Monadic version of 'or'. Evaluates the second part only if necessary.
 orM :: Monad m => m Bool -> m Bool -> m Bool
 orM m1 = ifM m1 (pure True)
 
+-- | Monadic version of 'and'. Evaluates the second part only if necessary.
 andM :: Monad m => m Bool -> m Bool -> m Bool
 andM m1 m2 = ifM m1 m2 (pure False)
 
-rightToMaybe :: Either a b -> Maybe b
-rightToMaybe =
-  \case
-    Right b -> Just b
-    Left _ -> Nothing
-
-predecessorRelation :: Ord a => (a -> Set a) -> Set a -> Map a (Set a)
-predecessorRelation succ base =
-  let init = Map.fromSet (const Set.empty) base
-   in foldl
-        (\m src ->
-           foldl (\m targ -> Map.insertWith Set.union targ (Set.singleton src) m) m (succ src))
-        init
-        base
-
-invertMap :: (Ord a, Ord k) => Map k a -> Map a (Set k)
-invertMap mp = Map.fromListWith Set.union $ map (\(k, a) -> (a, Set.singleton k)) $ Map.toList mp
-
-reachables :: Ord a => (a -> Set a) -> Set a -> Set a
-reachables succ = go Set.empty . OL.fromSet
-  where
-    go explored ol =
-      case OL.pop ol of
-        Nothing -> explored
-        Just (a, ol)
-          | a `elem` explored -> go explored ol
-          | otherwise -> go (Set.insert a explored) (OL.push (succ a `Set.difference` explored) ol)
-
+---------------------------------------------------------------------------------------------------
+-- String related
+---------------------------------------------------------------------------------------------------
+-- | The first line of a string. Returns the empty string if the input string is empty.
 firstLine :: String -> String
 firstLine str =
   case lines str of
     [] -> []
     s:_ -> s
 
-intmapSet :: (Ord a, Ord b) => (Integer -> a -> b) -> Set a -> [b]
-intmapSet mp = zipWith mp [0 ..] . Set.toList
+-- | Concatenates a list of 'String's with adding a separator between each of them. For example,
+-- 'inbetween "," ["a", "b", "c"] == "a,b,c"'
+inbetween :: String -> [String] -> String -- TODO: check usage!
+inbetween sep =
+  \case
+    [] -> []
+    [s] -> s
+    s:sr -> s ++ sep ++ inbetween sep sr
 
-mapFromSet :: (Ord a, Ord b) => Set (a, b) -> Map a b
-mapFromSet = Map.fromList . Set.toList
+-- | Encloses a string into some kind of brackets specified by the 'Char'. Currently
+-- '()[]{}' are supported for proper enclosure. If the 'Char' is not in this list,
+-- then it will be appended and prepended to the String.
+enclose :: Char -> String -> String -- TODO: check usage!
+enclose c str =
+  case c of
+    '(' -> "(" ++ str ++ ")"
+    ')' -> "(" ++ str ++ ")"
+    '[' -> "[" ++ str ++ "]"
+    ']' -> "[" ++ str ++ "]"
+    '{' -> "{" ++ str ++ "}"
+    '}' -> "{" ++ str ++ "}"
+    _ -> c : str ++ [c]
 
-mapFromSetWith :: (Ord k, Ord a) => (a -> a -> a) -> Set (k, a) -> Map k a
-mapFromSetWith f = Map.fromListWith f . Set.toList
+-- | Works like 'inbetween' and encloses the result in parenthesis.
+paraInbetween :: String -> [String] -> String -- TODO: check usage!
+paraInbetween sep elems = enclose '(' (inbetween sep elems)
 
+---------------------------------------------------------------------------------------------------
+-- Running things
+---------------------------------------------------------------------------------------------------
+-- | Given an (optional) timeout in seconds, runs a command with a list of command line
+-- arguments, and input on stdin, discarding the error code and stderr. If a timeout occurred
+-- 'runTO' will return 'Nothing', otherwise 'Just' with the content of stdout.
 runTO :: Maybe Int -> String -> [String] -> String -> IO (Maybe String)
 runTO to cmd args input =
   case to of
@@ -134,6 +164,7 @@ runTO to cmd args input =
           Just (_, res, _) -> return (Just res)
           Nothing -> pure Nothing
 
+-- | Removes the timeout of 'runTO'. If a timeout occured this will be an error.
 noTimeout :: IO (Maybe a) -> IO a
 noTimeout comp = do
   res <- comp
@@ -141,27 +172,47 @@ noTimeout comp = do
     Just res -> pure res
     Nothing -> error "assumed computation could not timeout!"
 
--- TODO: check usage!
-paraInbetween :: String -> [String] -> String
-paraInbetween sep elems = enclose '(' (inbetween sep elems)
+---------------------------------------------------------------------------------------------------
+-- Sets, Maps, Graphs
+---------------------------------------------------------------------------------------------------
+-- | Maps the element of a set to an list with additional indices from [0 to size - 1] in the
+-- ascending order of the elements of the set
+intmapSet :: (Ord a, Ord b) => (Integer -> a -> b) -> Set a -> [b]
+intmapSet mp = zipWith mp [0 ..] . Set.toList
 
--- TODO: check usage!
-inbetween :: String -> [String] -> String
-inbetween sep =
-  \case
-    [] -> []
-    [s] -> s
-    s:sr -> s ++ sep ++ inbetween sep sr
+-- | Inverts a 'Map' such that all elements of the input 'Map', map to all their keys in
+-- the input map
+invertMap :: (Ord a, Ord k) => Map k a -> Map a (Set k)
+invertMap mp = Map.fromListWith Set.union $ map (\(k, a) -> (a, Set.singleton k)) $ Map.toList mp
 
--- TODO: check usage!
-enclose :: Char -> String -> String
-enclose c str =
-  case c of
-    '(' -> "(" ++ str ++ ")"
-    ')' -> "(" ++ str ++ ")"
-    '[' -> "[" ++ str ++ "]"
-    ']' -> "[" ++ str ++ "]"
-    '{' -> "{" ++ str ++ "}"
-    '}' -> "{" ++ str ++ "}"
-    _ -> c : str ++ [c]
+-- | Shortcut for turning a set of tuples into a list and then into a map with 'Map.fromList'
+mapFromSet :: (Ord a, Ord b) => Set (a, b) -> Map a b
+mapFromSet = Map.fromList . Set.toList
+
+-- | Shortcut for turning a set of tuples into a list and then into a map with 'Map.fromListWith'
+mapFromSetWith :: (Ord k, Ord a) => (a -> a -> a) -> Set (k, a) -> Map k a
+mapFromSetWith f = Map.fromListWith f . Set.toList
+
+-- | Given a successors relation (in a directed graph) and a set of initial vertices
+-- 'reachables' computes the set of reachable vertices
+reachables :: Ord a => (a -> Set a) -> Set a -> Set a
+reachables succ = go Set.empty . OL.fromSet
+  where
+    go explored ol =
+      case OL.pop ol of
+        Nothing -> explored
+        Just (a, ol)
+          | a `elem` explored -> go explored ol
+          | otherwise -> go (Set.insert a explored) (OL.push (succ a `Set.difference` explored) ol)
+
+-- | Given a successor relation (in a directed graph) and a set of vertices, compute the
+-- predecessors relation for those vertices
+predecessorRelation :: Ord a => (a -> Set a) -> Set a -> Map a (Set a)
+predecessorRelation succ base =
+  let init = Map.fromSet (const Set.empty) base
+   in foldl
+        (\m src ->
+           foldl (\m targ -> Map.insertWith Set.union targ (Set.singleton src) m) m (succ src))
+        init
+        base
 ---------------------------------------------------------------------------------------------------
--- src/lib/Issy/Utils/OpenList.hs
+++ src/lib/Issy/Utils/OpenList.hs
@@ -1,13 +1,16 @@
 ---------------------------------------------------------------------------------------------------
 -- |
 -- Module      : Issy.Utils.OpenList
--- Description : TODO DOCUMENT
+-- Description : Simple open-list data structure for ordered types
 -- Copyright   : (c) Philippe Heim, 2026
 -- License     : The Unlicense
 --
+-- This module implements a simple FIFO open-list data structure for types that implement 'Ord'
+-- that can be used for search algorithms.
 ---------------------------------------------------------------------------------------------------
 {-# LANGUAGE Safe, LambdaCase #-}
 
+---------------------------------------------------------------------------------------------------
 module Issy.Utils.OpenList
   ( OpenList
   , pop
@@ -17,46 +20,49 @@ module Issy.Utils.OpenList
   , empty
   , fromSet
   , fromList
-  , toSet
-  , toList
   ) where
 
+---------------------------------------------------------------------------------------------------
 import Data.Set (Set)
 import qualified Data.Set as Set
 
+---------------------------------------------------------------------------------------------------
+-- | An open-list over types that implements 'Ord'. The list is a FIFO queue for new elements.
+-- Elements that are already queue are ignored, i.e. each element appears at most once.
 newtype OpenList a =
-  OpenList ([a], Set a)
+  OpenList ([a], Set a) -- TODO: Use the Queue instead of a list here for performance
   deriving (Eq, Ord, Show)
 
+-- | The empty open-list
+empty :: Ord a => OpenList a
+empty = OpenList ([], Set.empty)
+
+-- | Return and remove the first element in the open-list, if the open-list is not empty
 pop :: Ord a => OpenList a -> Maybe (a, OpenList a)
 pop =
   \case
     OpenList ([], _) -> Nothing
     OpenList (o:or, s) -> Just (o, OpenList (or, o `Set.delete` s))
 
+-- | Add a single element to the open-list
 pushOne :: Ord a => a -> OpenList a -> OpenList a
 pushOne new ol = Set.singleton new `push` ol
 
+-- | Add a set of element to the open-list in some undefined order
 push :: Ord a => Set a -> OpenList a -> OpenList a
 push new (OpenList (list, set)) =
   let reallyNew = new `Set.difference` set
-   in OpenList (list ++ Set.toList reallyNew, set `Set.union` reallyNew)
+   in OpenList (list ++ Set.toList reallyNew, set `Set.union` reallyNew) -- TODO make better order
 
+-- | Add a list of elements to the open-list in some undefined order
 pushList :: Ord a => [a] -> OpenList a -> OpenList a
-pushList = push . Set.fromList
+pushList = push . Set.fromList -- TODO make better order
 
-empty :: Ord a => OpenList a
-empty = OpenList ([], Set.empty)
-
+-- | Creates an open-list from a set of elements
 fromSet :: Ord a => Set a -> OpenList a
 fromSet set = set `push` empty
 
+-- | Creates an open-list from a list of elements
 fromList :: Ord a => [a] -> OpenList a
 fromList = foldl (flip pushOne) empty
-
-toSet :: Ord a => OpenList a -> Set a
-toSet (OpenList (_, s)) = s
-
-toList :: Ord a => OpenList a -> [a]
-toList (OpenList (l, _)) = l
 ---------------------------------------------------------------------------------------------------
