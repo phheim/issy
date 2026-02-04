29c6304c45c22577d37f94ec1aac378b8821e642
Philippe Heim <philippe.heim@cispa.de>
2026-02-04 21:52:35 +0100

    documentation

--- src/lib/Issy/Games/SymbolicState.hs
+++ src/lib/Issy/Games/SymbolicState.hs
@@ -1,10 +1,13 @@
 ---------------------------------------------------------------------------------------------------
 -- |
 -- Module      : Issy.Games.SymbolicState
--- Description : TODO DOCUMENT
+-- Description : Implementation of a symbolic state
 -- Copyright   : (c) Philippe Heim, 2026
 -- License     : The Unlicense
 --
+-- This module implements a symbolic states in the context of RPG / symbolic game solving.
+-- A symbolic state maps each locations in an RPG or symbolic game to a first-order logic
+-- formula, which represents then a set of states.
 ---------------------------------------------------------------------------------------------------
 {-# LANGUAGE Safe #-}
 
@@ -12,20 +15,19 @@
 module Issy.Games.SymbolicState
   ( SymSt
   , symSt
+  , null
   , get
   , set
+  , map
+  , mapWithLoc
   , disj
   , disjS
   , difference
-  , implies
-  , restrictTo
-  , locations
-  , toList
-  , null
   , simplify
-  , map
-  , mapWithLoc
+  , implies
+  , locations
   , symbols
+  , toList
   , toString
   ) where
 
@@ -49,68 +51,83 @@ import Issy.Utils.Logging
 -------------------------------------------------------------------------------
 -- Symbolic state
 -------------------------------------------------------------------------------
+-- | The representation of a symbolic state. A symbolic state is only
+-- well-defined in the context of a set of given locations usually provided
+-- by some kind of symbolic arena or location store.
 newtype SymSt =
   SymSt (Map Loc Term) -- assert that all location are mapped
   deriving (Eq, Ord, Show)
 
-liftSt :: (Map Loc Term -> a) -> SymSt -> a
-liftSt f (SymSt s) = f s
-
-mapSt :: (Map Loc Term -> Map Loc Term) -> SymSt -> SymSt
-mapSt f (SymSt s) = SymSt $ f s
-
+-- | Create a symbolic state for a set of locations.
+-- It is required that that the given set of location is complete within the
+-- context. Otherwise, operations on this symbolic state are undefined.
 symSt :: Set Loc -> (Loc -> Term) -> SymSt
 symSt locs gen = SymSt $ Map.fromSet gen locs
 
-get :: SymSt -> Loc -> Term
-get (SymSt s) l = fromMaybe (error "Assertion: All locations should be mapped") (s !? l)
+-- | Check if the symbolic state represent the trivial empty set, i.e. the
+-- formula for all locations are syntactically false.
+null :: SymSt -> Bool
+null = all ((== FOL.false) . snd) . toList
 
+-- | Get the value of the symbolic state at a given location. This location has
+-- to be part of the aforementioned context.
+get :: SymSt -> Loc -> Term
+get (SymSt s) l = fromMaybe (error "assert: all locations should be mapped") (s !? l)
+
+-- | Set the value of the symbolic state at a given location. This location has
+-- to be part of the aforementioned context.
 set :: SymSt -> Loc -> Term -> SymSt
 set (SymSt s) l f = SymSt $ Map.insert l f s
 
-disj :: SymSt -> Loc -> Term -> SymSt
-disj s l f = set s l $ FOL.orf [f, s `get` l]
+-- | Map all terms in a symbolic state.
+map :: (Term -> Term) -> SymSt -> SymSt
+map = mapSt . Map.map
 
+-- | Map all terms in a symbolic state with the location.
+mapWithLoc :: (Loc -> Term -> Term) -> SymSt -> SymSt
+mapWithLoc = mapSt . Map.mapWithKey
+
+-- | The location-wise disjunction of two symbolic states.
 disjS :: SymSt -> SymSt -> SymSt
 disjS (SymSt a) b = SymSt $ Map.mapWithKey (\l f -> FOL.orf [f, b `get` l]) a
 
+-- | The location-wise difference of two symbolic states.
 difference :: SymSt -> SymSt -> SymSt
 difference (SymSt a) b = SymSt $ Map.mapWithKey (\l f -> FOL.andf [f, FOL.neg (b `get` l)]) a
 
+-- | The disjunction of a term and the value of a symbolic state at a given location.
+disj :: SymSt -> Loc -> Term -> SymSt
+disj s l f = set s l $ FOL.orf [f, s `get` l]
+
+-- | Check if one symbolic state represent a subset of the other one. Note that
+-- this check will invoke an SMT solver.
 implies :: Config -> SymSt -> SymSt -> IO Bool
 implies cfg (SymSt a) b =
   SMT.valid cfg $ FOL.andf $ (\l -> (SymSt a `get` l) `FOL.impl` (b `get` l)) <$> Map.keys a
 
-locations :: SymSt -> [Loc]
-locations = liftSt Map.keys
-
-toList :: SymSt -> [(Loc, Term)]
-toList = liftSt Map.toList
-
-null :: SymSt -> Bool
-null = all ((== FOL.false) . snd) . toList
-
+-- | Apply simplifications to a symbolic state. This operation will invoke an SMT solver.
 simplify :: Config -> SymSt -> IO SymSt
 simplify cfg (SymSt s) = SymSt <$> mapM (SMT.simplify cfg) s
 
-map :: (Term -> Term) -> SymSt -> SymSt
-map = mapSt . Map.map
+-- | The locations of the symbolic state.
+locations :: SymSt -> [Loc]
+locations = liftSt Map.keys
 
-mapWithLoc :: (Loc -> Term -> Term) -> SymSt -> SymSt
-mapWithLoc = mapSt . Map.mapWithKey
+-- | A list representation of the symbolic state.
+toList :: SymSt -> [(Loc, Term)]
+toList = liftSt Map.toList
 
+-- | All symbols that appear in the symbolic state.
+symbols :: SymSt -> Set Symbol
+symbols = liftSt $ Set.unions . fmap FOL.symbols . Map.elems
+
+-- | Pretty print a symbolic state.
 toString :: (Loc -> String) -> SymSt -> String
 toString = liftSt . flip strM SMTLib.toString
 
-restrictTo :: Set Loc -> SymSt -> SymSt
-restrictTo locs =
-  mapSt
-    $ Map.mapWithKey
-        (\l v ->
-           if l `elem` locs
-             then v
-             else FOL.false)
+mapSt :: (Map Loc Term -> Map Loc Term) -> SymSt -> SymSt
+mapSt f (SymSt s) = SymSt $ f s
 
-symbols :: SymSt -> Set Symbol
-symbols = liftSt $ Set.unions . fmap FOL.symbols . Map.elems
+liftSt :: (Map Loc Term -> a) -> SymSt -> a
+liftSt f (SymSt s) = f s
 ---------------------------------------------------------------------------------------------------
